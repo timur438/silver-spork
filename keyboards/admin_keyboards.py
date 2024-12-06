5b6a621eb665f6b2fb2aae63da132d7c859c89df
timur438 <67786039+timur438@users.noreply.github.com>
2024-12-06 16:27:58 +0000

    modified:   keyboards/admin_keyboards.py

--- keyboards/admin_keyboards.py
+++ keyboards/admin_keyboards.py
@@ -14,7 +14,7 @@ role_4_admin_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="💳 Добавить кэшера"), KeyboardButton(text="🗑 Удалить кэшера")],
         [KeyboardButton(text="🏦 Добавить админа"), KeyboardButton(text="🗑 Удалить админа")],
-        [KeyboardButton(text="📊 Действия пользователя"), KeyboardButton(text="👤 Профиль пользователя")],
+        [KeyboardButton(text="📊 Действия пользователя"), KeyboardButton(text="👤 Профиль пользователя")], 
         [KeyboardButton(text="🔒 Сменить пароль")],
         [KeyboardButton(text="🔙 Назад")],
     ],
